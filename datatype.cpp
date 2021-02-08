fb56b53b6fa33af8381fed58ab231931df08483c
HimanthSura <78466704+HimanthSura@users.noreply.github.com>
2021-02-08 23:01:45 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+main()
+{
+    int a;
+    a=22;
+    cout<<"size of int"<<sizeof(a)<<endl;
+    float b;
+    cout<<"size of float"<<sizeof(b)<<endl;
+    char c;
+    cout<<"size of char"<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+    short int si;
+    long int li;
+    cout<<"size of shortint"<<sizeof(si)<<endl;
+    cout<<"size of longint"<<sizeof(li)<<endl;
+    /* code */
+    return 0;
+}
+
+// namespace std
